7569ec829e9e99851a1bf44a0080a8711d191661
changji.hd <changji.hd@alibaba-inc.com>
2015-02-05 14:33:26 +0800

    new

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,8 +4,10 @@
 (load "~/.emacs.d/themes/color-theme-molokai.el")
 (color-theme-molokai)
 ;;font
-(set-default-font "Monospace-12")
-(set-fontset-font "fontset-default" 'unicode '("WenQuanYi Micro Hei" . "unicode-ttf"))
+;;(set-default-font "Monospace-12")
+(set-frame-font "DejaVu Sans Mono-12")
+;;(set-fontset-font "fontset-default" 'unicode '("WenQuanYi Micro Hei" . "unicode-ttf"))
+(set-fontset-font "fontset-default" 'han "WenQuanYi Micro Hei")
 (setq-default cursor-type 'bar)
 
 
